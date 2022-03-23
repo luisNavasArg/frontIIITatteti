9ba16fd3a12d30da37b8a6b6fc49b96e8d9efe61
luisNavasArg <lpipnavas@gmail.com>
2022-03-23 14:32:52 -0300

    tateti

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import './App.css'
+function Square(props){
+  return(
+    <button className='square' onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
+class Board extends React.Component{
+  renderSquare(i){
+    return(
+      <Square
+        value={this.props.squares[i]}
+        onClick={()=>this.props.onClick(i)}
+      />
+    )
+  }
+  render(){
+    return(
+      <div>
+        <div className='board-row'>
+         { this.renderSquare(0)}
+         { this.renderSquare(1)}
+         { this.renderSquare(2)}
+        </div>
+        <div className='board-row'>
+         { this.renderSquare(3)}
+         { this.renderSquare(4)}
+         { this.renderSquare(5)}
+        </div>
+        <div className='board-row'>
+         { this.renderSquare(6)}
+         { this.renderSquare(7)}
+         { this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+class Game extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      history:[
+        {squares:Array(9).fill(null)}
+      ],
+      stepNumber:0,
+      xIsNext:false
+    }
+  }
+  handleClick(i){
+    const history= this.state.history.slice(0,this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    console.log(this.state.stepNumber)
+    squares[i]= this.state.xIsNext ? "X": "O";
+    this.setState(
+      {history:history.concat(
+        [{squares:squares}]
+      ),
+      stepNumber:history.length,
+      xIsNext : !this.state.xIsNext
+    }
+    )
+  }
+
+  render(){
+    let history = this.state.history;
+    let current = history[this.state.stepNumber]
+    let status;
+    status = "Next Juagdor" + (this.state.xIsNext ? "X":"O")
+    
+  return(
+    <div className='game'>
+      <div className='game-board'>
+      <Board
+        squares={current.squares}
+        onClick={i=> this.handleClick(i)}
+      />
+      </div>
+    </div>
+    
+  )
+}
+}
+
+export default Game
